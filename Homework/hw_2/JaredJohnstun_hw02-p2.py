f15c7ab4b6cc35309d8090bc4e28b043f66b5219
JJohnstun <jjohnstun@csus.edu>
2024-09-10 23:50:40 -0700

    pushing from deskop 9/11/2024

--- Homework/hw_2/JaredJohnstun_hw02-p2.py
+++ Homework/hw_2/JaredJohnstun_hw02-p2.py
@@ -9,41 +9,82 @@ x0 = 0 #initial x position
 y0 = 0 #initial y position
 v0 = 20 #initial velocity
 θ0 = 45 #inital angle of velocity relative to horizonal (in degrees) needs to be greater than zero
-g = -9.8 #acceleration due to gravity (m/s^2)
+g = 9.8 #acceleration due to gravity (m/s^2)
 
 #define functions for x position, y position, and range of projectile
 def x(t,θ):
     #convert θ from degrees to radians
-    np.deg2rad(θ)
-    x = x0 + (v0*t*np.cos(θ))
+    angle = np.deg2rad(θ)
+    x = x0 + (v0*t*np.cos(angle))
     return x
 
 def y(t,θ):
     #convert θ from degrees to radians
-    np.deg2rad(θ)
-    y = y0 + (v0*t*np.sin(θ)) - (0.5*g*t**2)
+    angle = np.deg2rad(θ)
+    y = y0 + ((v0*t)*(np.sin(angle))) - (0.5*g*(t*t))
     return y
 
 def Range(θ):
-    R = ((v0**2)/g)*np.sin(2*θ)
+    angle = np.deg2rad(θ)
+    R = ((v0**2)/g)*np.sin(2*angle)
     return R
 
 #Create function to generate values of t for the given information:
-def t_range():
-    t=0,
+def t_range(θ):
+    #start from t = 0
+    t = 0
     counter = 0
-    while y(t,θ0) >0 :
-        t +=0.2
+    while y(t, θ) >= 0:
+        t +=0.05
         counter +=1
-        return np.linspace(0, t, counter)
-    
-#Create First Plot
-θ_range = np.linspace(30, 60, 8)
+        continue
+    values = np.linspace(0, t, counter)
+    return values
 
+
+
+#Create First Plot
+θ_range = np.linspace(30, 60, 7)
+
+#Array of colors to iterate over when making graphs
+colors = ['magenta', 'r', 'y', 'g', 'c', 'b', 'purple']
+i=0 # pointer to Color array index
 for θ in θ_range:
     #generate range of t values
-    t_in = t_range()
-    #plug them into functions to get 
+    t_in = t_range(θ)
+    #generate name of curve
+    curve_angle = str(θ)
     
+    plt.plot(
+        x(t_in, θ), #Collection of x values for given t
+        y(t_in, θ), #Collection of y values for given t
+        color = colors[i],
+        linestyle = "-",
+        label = "θ initial (deg) = " + curve_angle
+
+    )
+    #increment Colors array index
+    i += 1
+
+    #iterate through angles and plot Range values from Range Function
+    plt.plot(
+        Range(θ), # given x value of plot
+        0,
+        markeredgecolor = "black",
+        marker = "+"
+    )
+ 
+
+
+plt.xlabel('X-position (m)')
+plt.ylabel('Y-position (m)')
+plt.title('Comparisons of x-y position and Range Functions')
+plt.legend()
+plt.xlim(0)
+plt.ylim(0)
+print("Showing plot window.")	
+print("Nothing else will happen until you close the plot window!")
+plt.savefig("Homework/Hw_2/JaredJohnstun_hw02-p3_image.pdf")
+plt.show()
 
 
