b0e75885b97043b79e776f4e64cbde94b8176f9b
JJohnstun <jjohnstun@csus.edu>
2024-12-09 23:09:45 -0800

    python file can read text files but diff is wrong

--- Final/Final-JaredJohnstun-plots.py
+++ Final/Final-JaredJohnstun-plots.py
@@ -1,2 +1,37 @@
 #Jared Johnstun
 #Date created: 12/9/2024
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+time = np.loadtxt('freq - 40000 - trans.txt', skiprows=1, usecols=0)
+Vr = np.loadtxt('freq - 40000 - trans.txt', skiprows=1, usecols=2)
+Vc = np.loadtxt('freq - 40000 - trans.txt', skiprows=1, usecols=3)
+Vl = np.loadtxt('freq - 40000 - trans.txt', skiprows=1, usecols=4)
+y_values = [Vr, Vc, Vl]
+colors = ['blue', 'red', 'green']
+
+
+
+
+
+
+#create figure
+plt.figure()
+
+#create subplot for each graph
+elem_trans = plt.subplot2grid((2, 2), (0, 0), rowspan=1, colspan=1)
+elem_steady = plt.subplot2grid((2, 2), (0, 1), rowspan=1, colspan=1)
+volt_trans = plt.subplot2grid((2, 2), (1, 0), rowspan=1, colspan=1)
+volt_steady = plt.subplot2grid((2, 2), (1, 1), rowspan=1, colspan=1)
+
+for i in range(0,2):
+    elem_trans.plot(
+        time,
+        y_values[i],
+        color = colors[i],
+        linestyle = '-'
+    )
+plt.show()
